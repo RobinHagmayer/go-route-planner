0f1034dc8d67accd4902aaf10fea76f3266d1b92
Robin Hagmayer <robin.hagmayer@proton.me>
2025-05-27 20:51:04 +0200

    feat: implemented custom logger

---
+++ internal/logger/logger.go
@@ -0,0 +1,21 @@
+package logger
+
+import (
+	"fmt"
+	"log/slog"
+	"os"
+)
+
+var Log *slog.Logger
+
+func Init() {
+	file, err := os.OpenFile("go-route-planner.log", os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0o644)
+	if err != nil {
+		fmt.Println("Error while opening the log file: %w", err)
+		os.Exit(1)
+	}
+
+	handler := slog.NewTextHandler(file, nil)
+	Log = slog.New(handler)
+	slog.SetDefault(Log)
+}
--- main.go
+++ main.go
@@ -1,7 +1,14 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log/slog"
+
+	"github.com/RobinHagmayer/go-route-planner/internal/logger"
+)
 
 func main() {
+	logger.Init()
+	slog.Info("App started")
 	fmt.Println("Hello go-route-planner")
 }
